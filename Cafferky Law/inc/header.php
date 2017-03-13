0605e68b48a602564cf1b006d45632124c5ac99f
Jonathan Shull <shulljw@gmail.com>
2017-03-13 13:08:59 -0700

    Added the in progress project files via upload
    included page, style and image files

---
+++ Cafferky Law/inc/header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+
+<html>
+  <head>
+    <meta charset="utf-8" name="viewport" content="width=device-width">
+    <title>Home</title><!-- convert to PHP $pageTitle variable later -->
+    <!-- google fonts --><link href="https://fonts.googleapis.com/css?family=Open+Sans|Sorts+Mill+Goudy" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+      rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css" type="text/css">
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <!-- javascript at bottom or use document load -->
+  </head>
+  
+  <body>
+    <div class="header group">
+      <div class="wrapper">
+        <div class="phone-number">
+          <p>503-555-1234</p>
+        </div>
+        <div class="flex-header">
+          <div class="logo"><a href="index.php"><img src="img/SVG_file.svg" alt="logo" /></a></div>
+        <ul class="navigation">
+          <li class="selected">Estate Planning</li>
+          <li>Business Law</li>
+          <li>Probate</li>
+          <li>Our Attorneys</li>
+        </ul>
+        </div>
+      </div>
+    </div><!-- end of .header -->
+    <div class="content">
---
+++ Cafferky Law/index.php
@@ -0,0 +1,67 @@
+    <?php include("inc/header.php"); ?>
+      <div class="top-spacer"></div>
+        <div class="jumbotron group">
+          <div class="wrapper">
+            <div class="contact-form">
+              <form method="post" action="hello.php">
+                <label for="name">Name:</label><input type="text" id="name" name="name">
+                <label for="email">Email:</label><input type="email" id="email" name="email">
+                <label for="phone">Phone Number:</label><input type="phone" id="phone" name="phone">
+                <label for="description">Description:</label>
+                <textarea id="description"></textarea>
+                <input type="submit" id="submit" name="submit">
+              </form>
+            </div><!-- end .contact-form-->
+            <div class="jumbo-text">
+              <h2>Legal Solutions for Business, Probate and Estate Planning Matters in Oregon and California</h2>
+              <p>Cafferkey Law Group offers legal advice with a focus on Business Law, Estate Planning, and Probate and Trust Administration. Our attorneys are licensed to practice in Oregon and California, providing our clients with the flexibility they need regarding local and interstate matters. Our qualified experience, attention and detail ensure our clients receive excellent legal advice.</p>
+            </div>
+          </div><!-- end .wrapper -->
+        </div><!-- end jumbotron -->
+      <div class="bottom-spacer group"></div>
+      
+      
+      <div class="section-two group">
+        <div class="wrapper">
+          <div class="checklist">
+            <h3 class="list-title">Business</h3>
+            <ul>
+              <li><i class="material-icons">done</i>Formation</li>
+              <li><i class="material-icons">done</i>Consultation</li>
+              <li><i class="material-icons">done</i>Contract Negotiation</li>
+              <li><i class="material-icons">done</i>Contract Review</li>
+              <li><i class="material-icons">done</i>Intellectual Property Enforcement</li>
+              <li><i class="material-icons">done</i>Litigation</li>
+              <li><i class="material-icons">done</i>Collections</li>
+            </ul>
+          </div><!-- end .checklist -->
+          <div class="checklist">
+            <h3 class="list-title">Estate Planning</h3>
+            <ul>
+              <li><i class="material-icons">done</i>Advanced Planning</li>
+              <li><i class="material-icons">done</i>Wills</li>
+              <li><i class="material-icons">done</i>Trusts</li>
+              <li><i class="material-icons">done</i>Special Needs Planning</li>
+              <li><i class="material-icons">done</i>Guardianships</li>
+              <li><i class="material-icons">done</i>Conservatorships</li>
+              <li><i class="material-icons">done</i>Probate</li>
+            </ul>
+          </div><!-- end .checklist -->
+          <p class="centered" style="clear: both">Cafferkey Law Group welcomes the opportunity to sit down with you to discuss the details of your unique situation.</p>
+        </div><!-- end .wrapper -->
+      </div><!-- end .section-two -->
+      
+    </div><!-- end .content -->
+    
+    
+    <footer>
+      <div class="disclaimer">
+        <div class="wrapper">
+          <p>Disclaimer: Attorneys of the Cafferky Law Group are licensed to practice law in Oregon and California. The information provided on this website is offered for informational purposes only. It is not offered as legal advice. Cafferky Law Group does not seek to represent you based upon your visit to or review of this website. An attorney-client relationship does not exist with Cafferky Law Group until your case has been evaluated and a written representation agreement has been entered into. This web site may be considered advertising under the rules of Oregon. You should not make legal hiring decisions based upon brochures, advertising, or other promotional materials.</p>
+        </div>
+      </div>
+      <p class="centered">Copyright Cafferky Law Group</p>
+    </footer>
+  </body>
+
+</html>
